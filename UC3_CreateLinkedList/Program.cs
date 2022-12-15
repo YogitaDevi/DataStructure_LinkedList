a1985c08c0307f9106c6767270e79a15337cdd2a
YogitaDevi <yogita.gry@gmail.com>
2022-12-15 19:21:44 +0530

    [yogita] create linked list

---
+++ UC3_CreateLinkedList/Program.cs
@@ -0,0 +1,28 @@
+﻿// C# program to illustrate how
+// to create a LinkedList
+using System;
+using System.Collections.Generic;
+
+public class UC3_CreateLinkedList
+{
+    static public void Main()
+    {
+
+        // Creating a linkedlist
+      
+        LinkedList<String> my_list = new LinkedList<String>();
+
+        my_list.AddLast("56");
+        my_list.AddLast("30");
+        my_list.AddLast("70");
+        
+
+        Console.WriteLine("Linked List are:");
+
+        foreach (string str in my_list)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
+
