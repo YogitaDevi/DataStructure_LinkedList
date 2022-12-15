4e13e63f40e002fb4faee4239e4eedcb5095d3e4
YogitaDevi <yogita.gry@gmail.com>
2022-12-15 19:14:39 +0530

    [yogita] insert elements

---
+++ UC4_InsertElement/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+public class UC4_InsertElement
+{
+    static public void Main()
+    {
+
+        LinkedList<String> my_list = new LinkedList<String>();
+
+        my_list.AddLast("56");
+        my_list.AddLast("30");
+        my_list.AddLast("70");
+        
+        Console.WriteLine("Sequence are: ");
+
+        foreach (string str in my_list)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
