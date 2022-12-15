f7110fac3c5a63f57a32b132461af269ab1b10d7
YogitaDevi <yogita.gry@gmail.com>
2022-12-15 19:06:16 +0530

    [yogita] last element delete

---
+++ UC6_LastDelete/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+public class UC6_LastDelete
+{
+    static public void Main()
+    {
+     
+        LinkedList<String> my_list = new LinkedList<String>();
+      
+        my_list.AddLast("56");
+        my_list.AddLast("30");
+        my_list.AddLast("70");
+        
+        Console.WriteLine("Linked List are: ");
+
+        foreach (string str in my_list)
+        {
+            Console.WriteLine(str);
+        }
+
+        Console.WriteLine("Final Sequence are: ");
+
+        my_list.RemoveLast();
+
+        foreach (string str in my_list)
+        {
+            Console.WriteLine(str);
+        }       
+    }
+}
+
